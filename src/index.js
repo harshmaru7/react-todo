826513b05386dc4c5b7837520ebf7a3328eb3e64
harshmaru7 <harsh.171it215@nitk.edu.in>
2021-10-05 00:03:17 +0530

    basic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'; install bootstrap node module first then import it 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
